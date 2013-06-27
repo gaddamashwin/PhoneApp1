f223412cedc8e235b2b96566911b4a3c698bb21c
Ashwin Gaddam <gaddam_ashwin@yahoo.com>
2013-06-27 19:51:59 -0400

    Done

--- PhoneApp1/MainPage.xaml.cs
+++ PhoneApp1/MainPage.xaml.cs
@@ -14,44 +14,30 @@ using System.Threading.Tasks;
 
 namespace PhoneApp1
 {
-    public class Testitems
-    {
-        public DateTime ItemId { get; set; }
-        public string ItemDescription { get; set; }
-        public string Link { get; set; }
-        public Brush ForegroundBrush { get; set; }
-        public bool IsEnabled { get; set; }
-
-        public Testitems(int id, string des, string link, Brush foregroundBrush, bool isEnabled)
-        {
-            this.ItemId = DateTime.Now;
-            this.ItemDescription = des;
-            this.Link = link;
-            this.ForegroundBrush = foregroundBrush;
-            this.IsEnabled = isEnabled;
-        }
-    }
-
     public partial class MainPage : PhoneApplicationPage
     {
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-
             this.DataContext = this;
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
-            CollectionItems = new List<Testitems>();
-            CollectionItems.Add(new Testitems(1, "Microsoft", "Sample3.wav", (Brush)Application.Current.Resources["PhoneContrastForegroundBrush"], false));
-            CollectionItems.Add(new Testitems(2, "Google", "Sample1.mp3", (Brush)Application.Current.Resources["PhoneForegroundBrush"], true));
-            CollectionItems.Add(new Testitems(3, "Apple", "Sample3.wav", (Brush)Application.Current.Resources["PhoneForegroundBrush"], true));
-            CollectionItems.Add(new Testitems(4, "Facebook", "Sample3.wav", (Brush)Application.Current.Resources["PhoneForegroundBrush"], true));
-            CollectionItems.Add(new Testitems(5, "Intel", "Sample3.wav", (Brush)Application.Current.Resources["PhoneForegroundBrush"], true));
-            CollectionItems.Add(new Testitems(6, "GE", "Sample3.wav", (Brush)Application.Current.Resources["PhoneForegroundBrush"], true));
-            CollectionItems.Add(new Testitems(7, "Linkedin", "Sample3.wav", (Brush)Application.Current.Resources["PhoneContrastForegroundBrush"], false));
+
+            ApplicationBar = new ApplicationBar();
         }
 
+        #region "Properties"
+        public List<PhoneServiceRef.FileContentColl> CollectionItems
+        {
+            get { return (List<PhoneServiceRef.FileContentColl>)GetValue(CollectionItemsProperty); }
+            set { SetValue(CollectionItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CollectionItems.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CollectionItemsProperty =
+            DependencyProperty.Register("CollectionItems", typeof(List<PhoneServiceRef.FileContentColl>), typeof(MainPage), new PropertyMetadata(null));
+        #endregion
+
+        #region "UI Events"
         private async void btnLogoff_Click_1(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/LoginPage.xaml", UriKind.Relative));
@@ -65,23 +51,24 @@ namespace PhoneApp1
             {
                 NavigationService.Navigate(new Uri("/LoginPage.xaml", UriKind.Relative));
             }
-            lstCollection.SelectedIndex = -1;
+            else
+            {
+                UpdateContentCollection(user.UserName);
+                //Set the lst collection index to -1 so that no list item is seleted by default
+                lstCollection.SelectedIndex = -1;
+            }
         }
-
-        public List<Testitems> CollectionItems { get; set; }
 
         private void lstCollection_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
             if (lstCollection.SelectedIndex != -1)
             {
-                Testitems item = (Testitems)e.AddedItems[0];
-                if (item.IsEnabled) NavigationService.Navigate(new Uri(string.Format("/Play.xaml?Description={0}&Link={1}", item.ItemDescription, item.Link), UriKind.Relative));
+                PhoneServiceRef.FileContentColl item = (PhoneServiceRef.FileContentColl)e.AddedItems[0];
+                if (string.IsNullOrEmpty(item.Filepath)) NavigationService.Navigate(new Uri(string.Format("/Play.xaml?Description={0}&Link={1}", item.ContentTitle, item.Filepath), UriKind.Relative));
                 else MessageBox.Show("Cannot play!");
             }
         }
 
-        //The foreground color of the text in SearchTB is set to Magenta when SearchTB
-        //gets focus.
         private void TB_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox txt = (TextBox)sender;
@@ -89,9 +76,6 @@ namespace PhoneApp1
             txt.Foreground = new SolidColorBrush((Color)Application.Current.Resources["PhoneTextBoxForegroundColor"]);
         }
 
-        //The foreground color of the text in SearchTB is set to Blue when SearchTB
-        //loses focus. Also, if SearchTB loses focus and no text is entered, the
-        //text "Search" is displayed.
         private void TB_LostFocus(object sender, RoutedEventArgs e)
         {
             TextBox txt = (TextBox)sender;
@@ -102,57 +86,115 @@ namespace PhoneApp1
             }
         }
 
-        private void btnSubmit_Click_1(object sender, RoutedEventArgs e)
+        private void SaveContent(object sender, EventArgs e)
         {
             try
             {
                 if (string.IsNullOrEmpty(Title.Text) || Title.Text == Title.Name) MessageBox.Show("Title is required.");
                 else if (string.IsNullOrEmpty(Content.Text) || Content.Text == Content.Name) MessageBox.Show("Content is required.");
-                else
-                {
-                    PhoneServiceRef.PhoneSvcClient svc = new PhoneServiceRef.PhoneSvcClient();
-                    svc.FileContentInsertCompleted += svc_FileContentInsertCompleted;
-                    //svc.GetDataCompleted += svc_GetDataCompleted;
-                    //svc.GetDataAsync(1);
-                    PhoneServiceRef.FileContentForInsert fileContent = new PhoneServiceRef.FileContentForInsert();
-                    fileContent.content = Content.Text;
-                    fileContent.speechRate = 3;
-                    fileContent.title = Title.Text;
-                    fileContent.userID = Security.GetUserInfo.UserName;
-                    fileContent.voiceID = 1;
-                    svc.FileContentInsertAsync(fileContent);
-                    
-                    svc.CloseAsync();
-                    // 
-                }
+                else saveContent(Content.Text, Title.Text);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-            
+        }
+
+        private void myPivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (myPivot.SelectedIndex == 2)
+            {
+                ApplicationBar.IsVisible = false;
+            }
+            else 
+            {
+                    ApplicationBar.IsVisible = true;
+                    if(ApplicationBar.Buttons.Count > 0) ApplicationBar.Buttons.RemoveAt(0);
+                    ApplicationBar.Buttons.Add(GetApplicationBarButton(myPivot.SelectedIndex));
+            }
+        }
+        #endregion
+
+        #region "Functions"
+
+        private ApplicationBarIconButton refreshContentbutton = null;
+        private ApplicationBarIconButton saveContentbutton = null;
+        public ApplicationBarIconButton GetApplicationBarButton(int PivotIndex)
+        {
+            if (PivotIndex == 0)
+            {
+                if (refreshContentbutton == null)
+                {
+                    refreshContentbutton = new ApplicationBarIconButton();
+                    refreshContentbutton.IconUri = new Uri("/Images/refresh.png", UriKind.Relative);
+                    refreshContentbutton.Text = "Refresh";
+                    refreshContentbutton.Click += new EventHandler(RefreshContent);
+                }
+                return refreshContentbutton;
+            }
+            else
+            {
+                if (saveContentbutton == null)
+                {
+                    saveContentbutton = new ApplicationBarIconButton();
+                    saveContentbutton.IconUri = new Uri("/Images/save.png", UriKind.Relative);
+                    saveContentbutton.Text = "Save";
+                    saveContentbutton.Click += new EventHandler(SaveContent);
+                }
+                return saveContentbutton;
+            }
+        }
+
+        void saveContent(string content, string title)
+        {
+            PhoneServiceRef.FileContentForInsert fileContent = new PhoneServiceRef.FileContentForInsert();
+            fileContent.content = content;
+            fileContent.speechRate = 3;
+            fileContent.title = title;
+            fileContent.userID = Security.GetUserInfo.UserName;
+            fileContent.voiceID = 1;
+
+            var svc = new PhoneServiceRef.PhoneSvcClient();
+            svc.FileContentInsertAsync(fileContent);
+            svc.FileContentInsertCompleted += svc_FileContentInsertCompleted;
+            svc.CloseAsync();
+
+            PhoneServiceRef.FileContentColl newFileContent = new PhoneServiceRef.FileContentColl();
+            newFileContent.ContentTitle = title;
+            newFileContent.CreatedDatetime = DateTime.Now;
+            CollectionItems.Add(newFileContent);
+        }
+
+        private void RefreshContent(object sender, EventArgs e)
+        {
+            var user = Security.GetUserInfo;
+            UpdateContentCollection(user.UserName);
+        }
+
+        void UpdateContentCollection(string userName)
+        {
+            var svc = new PhoneServiceRef.PhoneSvcClient();
+            //Get the data for the list
+            svc.FileContentMyCollSelectAllAsync(userName);
+            svc.FileContentMyCollSelectAllCompleted += svc_FileContentMyCollSelectAllCompleted;
+            svc.CloseAsync();
         }
 
         void svc_FileContentInsertCompleted(object sender, PhoneServiceRef.FileContentInsertCompletedEventArgs e)
         {
-            MessageBox.Show(e.Result);
+            //btnSubmit.IsEnabled = true;
+            //btnSubmit.Content = "Submit";
+            Content.Text = Content.Name;
+            Title.Text = Title.Name;
+            //myPivot.SelectedIndex = 0;
+            MessageBox.Show("Successfully submitted");
         }
 
+        void svc_FileContentMyCollSelectAllCompleted(object sender, PhoneServiceRef.FileContentMyCollSelectAllCompletedEventArgs e)
+        {
+            CollectionItems = e.Result.ToList();
+        }
+        #endregion
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
     }
 }
