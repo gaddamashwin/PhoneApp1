5eb2e6cf7351c137e4cdb3529a8c2c18b3a1b6db
gaddamashwin <gaddam_ashwin@yahoo.com>
2013-06-29 00:54:47 -0400

    All Changes.

--- PhoneService/ReadtoMeModel.Designer.cs
+++ PhoneService/ReadtoMeModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\asgaddam\Documents\GitHub\PhoneApp1\PhoneService\ReadtoMeModel.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\ashwin\Documents\GitHub\PhoneApp1\PhoneService\ReadtoMeModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
